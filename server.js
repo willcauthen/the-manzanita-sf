0ec4ac540f985d0ebd9848b68cf21297845bd4c8
Will Cauthen <willcauthen50@gmail.com>
2017-02-14 16:29:19 -0800

    declared port variable

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require('express'),
     house = roster.getHouse(),
     app = express();
 
+app.set('port', process.env.PORT || 3000);
+
 app.get('/', function (req, res) {
   res.send("Hello World!");
 });
@@ -12,6 +14,4 @@ app.get('/coffee', function(rew, res) {
   res.json({dates: house.coffee()});
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(app.get('port'));
