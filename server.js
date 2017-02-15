7eb475ad5ff05c04a31b4e3da869c6b1ae307346
Will Cauthen <willcauthen50@gmail.com>
2017-02-15 12:20:44 -0800

    testing slackbot integration

--- server.js
+++ server.js
@@ -9,10 +9,14 @@ app.get('/', function (req, res) {
 });
 
 app.get('/coffee', function(rew, res) {
-
+  house.shuffle();
   res.json({text: "_COFFEE DATES_",
             attachments: [
-              {text: "Date:" + house.roster[0] + " and " + house.roster[1]}
+              {text: "Date: " + house.roster[0] + " and " + house.roster[1]},
+              {text: "Date: " + house.roster[2] + " and " + house.roster[3]},
+              {text: "Date: " + house.roster[4] + " and " + house.roster[5]},
+              {text: "Date: " + house.roster[6] + " and " + house.roster[7]},
+              {text: "Date: " + house.roster[8] + ", " + house.roster[9] + ", and " + house.roster[10]},
             ]
   });
 });
